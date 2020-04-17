dc1d299b7f7258105577cbf702619eecb79a617b
josh-abrams <joshua.a.abrams@gmail.com>
2020-04-17 13:28:11 -0500

    Add functionality for player_words array to update

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -1,24 +1,39 @@
 extends Control
 
-func _ready():
-	var prompts = ["This", "Monkey", "Yum"]
-	var story = "%s is the biggest %s I've ever seen. %s"
+var player_words = []
+var story = "%s is the biggest %s I've ever seen. %s!"
+var prompts = ["a name", "a noun", "an expletive"]
 
+onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText
+onready var DisplayText = $VBoxContainer/DisplayText
 
-
-	
-	
+func _ready():	
+	check_player_words_length()
 
 func _on_PlayerText_text_entered(new_text):
-	update_DisplayText(new_text)
+	add_to_player_words()
 
 
 func _on_TextureButton_pressed():
-	var new_text = $VBoxContainer/HBoxContainer/PlayerText.text
-	update_DisplayText(new_text)
+	add_to_player_words()
 
 
-func update_DisplayText(new_text):
-	$VBoxContainer/DisplayText.text = new_text
-	$VBoxContainer/HBoxContainer/PlayerText.clear()
+func add_to_player_words():
+	player_words.append(PlayerText.text)
+	PlayerText.clear()
+	check_player_words_length()
 
+func is_story_done():
+	return player_words.size() == prompts.size()
+	
+func check_player_words_length():
+	if is_story_done():
+		tell_story()
+	else:
+		prompt_player()
+
+func tell_story():
+	DisplayText.text = story % player_words
+	
+func prompt_player():
+	DisplayText.text = "May I have " + prompts[player_words.size()] + " please?"
