96319fdd3541abc941d245bd7513477e42d93bb8
josh-abrams <joshua.a.abrams@gmail.com>
2020-04-20 09:42:09 -0500

    Refactor to allow story templates

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -1,21 +1,28 @@
 extends Control
 
 var player_words = []
-var story = "%s is the biggest %s I've ever seen. %s!"
-var prompts = ["a name", "a noun", "an expletive"]
+var current_story = {
+		"prompts" : ["a name", "a noun", "an expletive"],
+		"story": "%s is the biggest %s I've ever seen. %s!"
+		}
 
 onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText
 onready var DisplayText = $VBoxContainer/DisplayText
+onready var ConfirmButtonText = $VBoxContainer/HBoxContainer/ButtonLabel
 
 func _ready():	
 	check_player_words_length()
+	PlayerText.grab_focus()
 
 func _on_PlayerText_text_entered(new_text):
 	add_to_player_words()
 
 
 func _on_TextureButton_pressed():
-	add_to_player_words()
+	if is_story_done():
+		get_tree().reload_current_scene()
+	else:
+		add_to_player_words()
 
 
 func add_to_player_words():
@@ -24,16 +31,24 @@ func add_to_player_words():
 	check_player_words_length()
 
 func is_story_done():
-	return player_words.size() == prompts.size()
+	return player_words.size() == current_story.prompts.size()
 	
 func check_player_words_length():
 	if is_story_done():
-		tell_story()
+		end_game()
 	else:
 		prompt_player()
 
 func tell_story():
-	DisplayText.text = story % player_words
+	DisplayText.text = current_story["story"] % player_words
 	
 func prompt_player():
-	DisplayText.text = "May I have " + prompts[player_words.size()] + " please?"
+	DisplayText.text = "May I have " + current_story.prompts[player_words.size()] + " please?"
+
+func change_confirm_label():
+	ConfirmButtonText.text = "Woo!"
+
+func end_game():
+	PlayerText.queue_free()
+	change_confirm_label()
+	tell_story()
